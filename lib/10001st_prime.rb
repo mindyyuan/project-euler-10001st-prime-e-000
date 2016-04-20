8ab805a6543c45e2fc47472550629fb3e970c9ff
mindyyuan <mindyuan@gmail.com>
2016-04-19 20:54:38 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,27 @@
-# Implement your procedural solution here!
+require 'pry'
+
+def prime_number_for(num)
+  primes = [1, 2]
+  x = 3
+    while primes.length <= num
+      if is_prime?(x)
+        primes.push(x)
+      end
+      x+=1
+    end
+  primes[num]
+end
+
+def is_prime?(x)
+factor = 2
+  while factor < x
+    if x % factor == 0
+      return false
+    else
+      factor +=1
+    end
+  end
+  true
+end
+
+prime_number_for(12)
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,31 @@
-# Implement your object-oriented solution here!
+class Prime
+
+attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    primes = [1, 2]
+    x = 3
+      while primes.length <= num
+        if is_prime?(x)
+          primes.push(x)
+        end
+        x+=1
+      end
+    primes[num]
+  end
+
+  def is_prime?(x)
+  factor = 2
+    while factor < x
+      if x % factor == 0
+        return false
+      else
+        factor +=1
+      end
+    end
+    true
+  end
+end
